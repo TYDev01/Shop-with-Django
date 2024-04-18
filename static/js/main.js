c6862ce106429b4b9ffc905bdd32c4d489188453
Anthony <anthonychinaemeru84@yahoo.com>
2024-04-18 21:34:49 +0100

    Login added, users can login. will update the alert feature to toastify or switch to our own Js event

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+
+
+// let liveToast = document.getElementById("liveToastBtnm");
+// // console.log(liveToast)
+
+// liveToast.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     console.log("Clicked")
+// })
--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,3 +10,24 @@ def home(request):
 
 def about(req):
     return render(req, 'about.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Logged in successfully..."))
+            return redirect('home')
+        else:
+            messages.warning(request, ("error, incorrect details..."))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Logged out successfully..."))
+    return redirect('login')
+
