e34ea9870546afe59d586b8675c47beb54165b90
Anthony <anthonychinaemeru84@yahoo.com>
2024-04-19 08:19:21 +0100

    Registeration form added, users can easily create an account

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
 
 # Create your views here.
 def home(request):
@@ -31,3 +36,22 @@ def logout_user(request):
     messages.success(request, ("Logged out successfully..."))
     return redirect('login')
 
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Login User
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Account created successfully..."))
+            return redirect('home')
+        else:
+            messages.success(request, ("Fill up the required details"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
